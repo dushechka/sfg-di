c8f309dca321e89a4c09050e60e2f0b819788274
dushechka <aaafdv@gmail.com>
2021-11-03 22:37:49 +0300

    fix pet service

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
-@Service("cat")
+@Primary
+@Service()
+@Profile("cat")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
